245753acbf57fd970b46947775d7e16e2f484f16
Amir <amirnikravan@outlook.com>
2024-06-06 18:28:25 +0330

    add some feautres to line edit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox ,QLabel,QTableWidgetItem
-import sys
+import sys,os
 from PIL import Image
 import barcode.base
 import barcode.itf
@@ -23,6 +23,8 @@ class MainWindow(QMainWindow):
         self.ui.tableWidget.setColumnWidth(2,200)
         self.ui.tableWidget.setColumnWidth(3,450)
         self.ui.lineEdit.setFocus()
+        self.ui.lineEdit.setMaxLength(15)
+        self.ui.lineEdit.returnPressed.connect(self.barcode_scan)
     def showAboutMessageBox(self):
         msg = QMessageBox()
         msg.setWindowTitle("About")
@@ -33,6 +35,7 @@ class MainWindow(QMainWindow):
         # print(self.lineEdit.text())
         try:
             self.barcode_serial = self.ui.lineEdit.text()
+            self.ui.lineEdit.clear()
             barcode.base.Barcode.default_writer_options['write_text'] = False
             if self.barcode_serial:
                 barcode.base.Barcode.default_writer_options['text'] = f"IMEI={self.barcode_serial}"
@@ -80,80 +83,41 @@ class MainWindow(QMainWindow):
         self.ui.lineEdit.setFocus()
     def handlePaintRequest(self, printer):
         document = QtGui.QTextDocument()
-
-        # Create QTextCursor
         cursor = QtGui.QTextCursor(document)
-
-        # Calculate total number of barcodes
         total_barcodes = self.ui.tableWidget.rowCount()
-
-        # Display 10 barcodes per page
         barcodes_per_page = 10
-
-        # Calculate total number of pages needed
         total_pages = (total_barcodes + barcodes_per_page - 1) // barcodes_per_page
-
-        # Calculate number of rows and columns for the grid
         num_rows = 5
         num_cols = 2
-
-        # Iterate through each page
         for page_num in range(total_pages):
             # Start a new page
             cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
-
             cursor.insertBlock()
-
-            # Calculate the range of barcodes to print on this page
             start_index = page_num * barcodes_per_page
             end_index = min(start_index + barcodes_per_page, total_barcodes)
-
-            # Calculate the number of rows and columns needed for this batch of barcodes
             num_rows_batch = (end_index - start_index + num_cols - 1) // num_cols
             num_cols_batch = min(num_cols, end_index - start_index)
-
-            # Iterate through the rows
             for row_index in range(num_rows_batch):
-                # Create a table row
                 cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
-
                 cursor.insertBlock()
                 cursor.movePosition(QtGui.QTextCursor.MoveOperation.EndOfBlock)
-
                 cursor.insertText('\n')
-
-                # Iterate through the columns
                 for col_index in range(num_cols_batch):
                     barcode_index = start_index + row_index * num_cols_batch + col_index
                     if barcode_index < end_index:
-                        # Get the text of the barcode
                         item_text = self.ui.tableWidget.item(barcode_index, 0).text()
-
-                        # Generate barcode image
                         with open(f".\\images\\{item_text}.jpeg", "wb") as f:
                             Code128(item_text, writer=ImageWriter()).write(f)
-
-                        # Load the barcode image as QPixmap
                         pic = QtGui.QPixmap(f".\\images\\{item_text}.jpeg")
-
-                        # Convert QPixmap to QTextImageFormat
                         image_format = QtGui.QTextImageFormat()
                         image_format.setWidth(310)
                         image_format.setHeight(200)
                         image_format.setName(f".\\images\\{item_text}.jpeg")  # Set the image file path
-
-                        # Insert the image into the QTextDocument
                         cursor.insertImage(image_format)
-
-            # Move to the next page
             if page_num < total_pages - 1:
                 cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
-
                 cursor.insertBlock()
-
-        # Print the document
         printer.setOutputFormat(QtPrintSupport.QPrinter.OutputFormat.NativeFormat)
-
         document.print_(printer)
 
 if __name__ == "__main__":
